48cb7974d5f8a79a9b4159946bdc41b0a31ba06a
rohannss <shinderohan4619@gmail.com>
2021-09-15 01:09:11 +0530

    hii jack, this is my first commit to git

---
+++ src/samplepkg/Gitpush.java
@@ -0,0 +1,13 @@
+package samplepkg;
+
+public class Gitpush {
+
+	public static void main(String[] args) {
+	
+		System.out.println("This is my first git code");
+		
+		System.out.println("This is my second git code");
+
+	}
+
+}
